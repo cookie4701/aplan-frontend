195eb48955e03cbd80a1867340d1cf12447eb8f5
Paku <pascal@pkit.eu>
2021-02-17 14:38:21 +0100

    Modified config.js for testing purposes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-let baseURL = 'https://remote3.jugendbuero.be/arbeitsplan';
+//let baseURL = 'https://remote3.jugendbuero.be/arbeitsplan';
+let baseURL = 'http://localhost:8080';
 let orgacode = 'jbuero2020';
 
 export const apiHost = baseURL;
