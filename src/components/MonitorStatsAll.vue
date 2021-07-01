5f0d05455fe23751ce3bbe5274c640dc58f23dfb
Paku <pascal@pkit.eu>
2021-07-01 06:13:16 +0200

    Added file for moderation stats

---
+++ src/components/MonitorStatsAll.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                Überstunden Statistik aller Nutzer anzeigen
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                Hier kommt die Datenauswahl hin mit Button.
+            </div>
+        </div>
+
+        <div class="row">
+            <div id="zielbereich">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import {apiHost} from "../config";
+
+
+export default {
+    name : 'MonitorStatsAll',
+    data : {
+        return {
+            'var' : 'val',
+            'userdata' : []
+        }
+    },
+    methods : {
+        gatherDataControl() {
+
+        },
+        gatherUserList() {
+
+        },
+        gatherUserDetailData() {
+            
+        }
+    }
+}
+</script>
+
+
